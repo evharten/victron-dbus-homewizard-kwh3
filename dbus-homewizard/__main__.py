5e3af92bdf7f99f0c17687e305789bbb544748ae
Robin van der Gracht <robinvandergracht@gmail.com>
2024-09-27 21:31:54 +0200

    Display command line default values in help
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- dbus-homewizard/__main__.py
+++ dbus-homewizard/__main__.py
@@ -15,19 +15,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument('ip', metavar='IP', type=str,
     help="IP address of a Homewizard kWh meter")
 parser.add_argument('--role', default="pvinverter", choices=ROLES,
-    help="What is the source of the power the device measures")
+    help="What is the source of the power the device measures [default: %(default)s]")
 parser.add_argument('--instance', metavar='NUMBER', type=int, default=10,
-    help="Unique device instance number")
+    help="Unique device instance number [default: %(default)d]")
 parser.add_argument('--phase', type=int, default=1, choices=(1, 2, 3),
     help="Phase number we're monitoring (In case of single phase meter) [default: %(default)d]")
 parser.add_argument('--position', type=int, default=0, choices=(0, 1, 2),
     help="0=AC input 1; 1=AC output; 2=AC input 2  [default: %(default)d]")
 parser.add_argument('--name', default='homewizard',
-    help="Unique postfix for the dbus service name for separation and identification")
+    help="Unique postfix for the dbus service name for separation and identification [default: %(default)s]")
 parser.add_argument('--maxpower', type=int,
     help="Max rated power (in Watts) of the inverter")
 parser.add_argument('--pollinterval', type=float, default=1,
-    help="Poll interval in seconds. Should be no less than 0.5")
+    help="Poll interval in seconds. Should be no less than 0.5 [default: %(default).1f]")
 args = parser.parse_args()
 
 if not re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", args.ip):
